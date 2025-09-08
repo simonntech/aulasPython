c28b7b3ec7c8467e8d6c3450239bbec16663cb69
Simon Tech <contato.simon.tech@gmail.com>
2025-09-08 15:20:34 -0300

    aula01 unidade 02

---
+++ Unidade02/Aula01/listas.py
@@ -0,0 +1,4 @@
+cores = ['vermelho', 'azul', 'verde', 'amarelo', 'laranja']
+
+for cor in cores:
+    print(f'Posição {cores.index(cor)}, cor {cor}')
---
+++ Unidade02/Aula01/operações.py
@@ -0,0 +1,6 @@
+texto = "Explorando a diversidade de linguagens de programação com Python"
+
+print(f'Tamanho do texto = {len(texto)} caracteres')
+print(f'Python in texto = {'Python' in texto}')
+print(f'Quantidade de "e" no texto = {texto.count('e')}')
+print(f'As 5 primeiras letras são: {texto[:5]}')
