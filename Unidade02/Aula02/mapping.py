f7adaedf36cb273dad901b7f0d5498c3ead60c90
Simon Tech <contato.simon.tech@gmail.com>
2025-09-09 09:59:53 -0300

    dici2

--- Unidade02/Aula02/mapping.py
+++ Unidade02/Aula02/mapping.py
@@ -5,3 +5,10 @@ dici_1 = {}
 dici_1['nome'] = 'Brunão'
 dici_1['idade']= 35
 print(dici_1)
+
+#exemplo 2 de como criar atribuição
+dici_2 = {
+    'nome': 'Mariana', 
+    'idade': 27}
+print(dici_2)
+
