277842ffd25e66664f8f81c8166669bfe4faa752
Simon Tech <contato.simon.tech@gmail.com>
2025-09-09 10:12:04 -0300

    numPy

---
+++ Unidade02/Aula02/numPy.py
@@ -0,0 +1,24 @@
+import numPy as np
+
+array = np.array([1,2,3,4,5])
+
+print('Array original:')
+print(array)
+
+#operações matemáticas
+squared_array = array ** 2
+sum_elements = np.sum(my_array)
+
+print('\nArray ao quadrado:')
+
+print(squared_array)
+
+print('\nSoma dos elementos:')
+
+print(sum_of_elements)
+
+# Acesse elementos do array
+
+element_at_index_2 = my_array[2]  # Acessa o elemento no índice 2
+
+print('\nElemento no índice 2:', element_at_index_2)
