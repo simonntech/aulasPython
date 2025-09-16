1dcd9ea21c1cfb8af2ae0a21da5c749d6814bebe
Simon Tech <contato.simon.tech@gmail.com>
2025-09-16 10:46:27 -0300

    functions

--- Unidade02/Atividade Prática/sistemaBiblioteca.py
+++ Unidade02/Atividade Prática/sistemaBiblioteca.py
@@ -1,17 +1,48 @@
 import matplotlib.pyplot as plt
 
 class Livro:
-    def __init__(self, titulo, autor, genero, quantidadeDisponivel):
+    def __init__(self, titulo, autor, genero, quantidade):
         self.titulo = titulo
         self.autor = autor
         self.genero = genero
-        self.quantidadeDisponivel = quantidadeDisponivel
+        self.quantidade = quantidade
 
-listaLivros = Livro([])
+    def __str__(self):
+        return f'Título: {self.titulo}, Autor: {self.autor}, Gênero: {self.genero}, Quantidade: {self.quantidade}'
 
-print('Biblioteca de Livros')
-resposta = input('Vamos cadastrar um novo livro (ou aperte S para sair)')
+listaLivros = ([])
 
-while resposta <= 'S':
-    print('Cadastre um novo livro: \n')
-    nomeLivro = input('Nome do livro:')
+def cadastrarLivro():
+    print('--- Cadastro de Novo Livro ---')
+    titulo = input('Título do livro: ')
+    autor = input('Autor:')
+    genero = input('Gênero literário: ')
+    try:
+        quantidade = int(input('Quantidade disponível: '))
+        novoLivro = Livro(titulo, autor, genero, quantidade)
+        listaLivros.append(novoLivro)
+        print('\nLivro novo cadastrado!')
+    except ValueError:
+        print('\nErro: A quantidade deve ser um número inteiro.')
+
+def listarLivros():
+    print('\n Livros disponíveis:')
+    if not listaLivros:
+        print('Nenhum livro cadastrado até o momento...')
+    else:
+        for livro in listaLivros:
+            print(livro)
+
+def buscarPorTitulo():
+    print('\n Buscar livro por título:')
+    tituloBusca = input('Nome do livro:')
+    encontrado = False
+
+    for livro in listaLivros:
+        if livro.titulo.lower() == tituloBusca.lower():
+            print('\nLivro encontrado: ')
+            print(livro)
+            encontrado = True
+            break
+        if not encontrado:
+            print(f'O livro "{tituloBusca}" não foi encontrado...')
