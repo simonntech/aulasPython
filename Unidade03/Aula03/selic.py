b107f4f16385930820be2507c5d4108b202a931e
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-09-17 20:12:35 -0300

    selic 2

--- Unidade03/Aula03/selic.py
+++ Unidade03/Aula03/selic.py
@@ -3,3 +3,14 @@ import pandas as pd
 df_selic = pd.read_json('https://api.bcb.gov.br/dados/serie/bcdata.sgs.11/dados?formato=json')
 
 print(df_selic.info())
+
+from datetime import date
+from datetime import datetime as dt
+
+data_extracao = date.today()
+
+df_selic['data_extracao']=data_extracao
+df_selic['responsavel']='Autor'
+
+print(df_selic.info())
+df_selic.head()
