8a7af9e194a6d0a2a16d7ad44fabfa0f665c3533
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-09-17 19:44:25 -0300

    pandas 1

---
+++ Unidade03/Aula02/dicionário.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+data = {
+    'A': 100, 
+    'B':200,
+    'C':300,
+    'D':400,
+    'E':500,
+    'F':600
+    }
+
+series2 = pd.Series(data)
+
+print(series2)
---
+++ Unidade03/Aula02/series.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+data = [10,20,30,40,50,60,70]
+
+series1 = pd.Series(data)
+
+print(series1)
