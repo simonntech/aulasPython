f838cb558610f08bb92017620904e36aaa8eeea0
Bruno Simon  Simon tech / <contato.simon.tech@gmail.com>
2025-09-20 15:19:54 -0300

    aula

---
+++ Unidade04/web.py
@@ -0,0 +1,13 @@
+from IPython.display import HTML
+
+html_code =
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Exemplo de front-end com Pyton </title>
+</head>
+<body>
+    <h1> Hello, world! </h1>
+    <p>Esta é uma página web criada com Python</p>
+</body>
+</html>
