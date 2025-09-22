1dc29ba0ed82bd82b7dac78f090bd92f227688af
Simon Tech <contato.simon.tech@gmail.com>
2025-09-22 09:16:35 -0300

    comit

--- Unidade04/Aula04/machineLearning.py
+++ Unidade04/Aula04/machineLearning.py
@@ -1,2 +1,19 @@
+import matplotlib.pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.models
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+X_train = tf.constant([[1.0], [2.0], [3.0], [4.0]])
+y_train = tf.constant([[2.0], [4.0], [6.0], [8.0]])
+
+model = Sequential()
+model.add(Dense(units=1, input_shape=(1,)))
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+model.fit(X_train, y_train, epochs=1000, verbose=0)
+
+X_new = tf.constant([[5.0]])
+prediction=model.predict(X_new)
+print('Predição:', prediction[0][0])
+plt.ylabel('Notas')
+plt.show()
